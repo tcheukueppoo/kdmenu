7eb25731ecf97c26cdf9e896ac26ceb057319266
tcheukueppo <tcheukueppo@tutanota.com>
2022-09-23 09:29:39 +0100

    Update color

--- config.h
+++ config.h
@@ -6,17 +6,17 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FantasqueSansMono Nerd Font:size=10"
+	"FantasqueSansMono Nerd Font:size=10.4"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#000000", "#d3d8ed" },
-	[SchemeSel] = { "#d3d8ed", "#31688b" },
-	[SchemeSelHighlight] = { "#d3d8ed", "#31688b" },
-	[SchemeNormHighlight] = { "#31688b", "#d3d8ed" },
-	[SchemeOut] = { "#32a883", "#d3d8ed" },
-	[SchemeOutHighlight] = { "#32a883", "#d3d8ed" },
+	[SchemeNorm] = { "#000000", "#bdae93" },
+	[SchemeSel] = { "#bdae93", "#458588" },
+	[SchemeSelHighlight] = { "#bdae93", "#458588" },
+	[SchemeNormHighlight] = { "#458588", "#bdae93" },
+	[SchemeOut] = { "#32a883", "#bdae93" },
+	[SchemeOutHighlight] = { "#32a883", "#bdae93" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
