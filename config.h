57d7dc465b2daee0095112d29c8543c238ec45b8
kueppo <tcheukueppo@yandex.com>
2023-08-21 17:47:56 +0100

    untrack *.o, update config.h

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#000000", "#bdae93" },
+	[SchemeNorm] = { "#ebdbb2", "#161616" },
 	[SchemeSel] = { "#000000", "#458588" },
 	[SchemeSelHighlight] = { "#bdae93", "#458588" },
 	[SchemeNormHighlight] = { "#458588", "#bdae93" },
